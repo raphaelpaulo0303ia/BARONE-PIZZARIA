e738c9cf73f77a0c48cdb652b4f2d5508c11180f
raphaelpaulo0303ia <raphaelpaulo0303@gmail.com>
2025-09-09 14:05:30 -0300

    Update main.jsx

--- barone-pizzaria/src/main.jsx
+++ barone-pizzaria/src/main.jsx
@@ -4,3 +4,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(console.error);
+  });
+}
